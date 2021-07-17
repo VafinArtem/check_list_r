0840bbac415d5a17d5601a49f9dc7441102942e3
Vafin Artem <a.vafin85@gmail.com>
2021-07-17 19:53:37 +0500

    Добавил компонент логин

---
+++ src/components/login/login.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const Login = () => {
+  return <main className="main main--login">
+    <h1 className="visually-hidden">Регистрация и авторизация</h1>
+    <section className="main__login login">
+      <div className="login__tabs">
+        <div className="login__tab-wrapper">
+          <button className="login__tab login__tab--active">Авторизация</button>
+        </div>
+        <div className="login__tab-wrapper">
+          <button className="login__tab">Регистрация</button>
+        </div>
+      </div>
+      <div className="login__container">
+        <div className="login__wrapper login__wrapper--active">
+          <h2 className="login__title">Авторизация</h2>
+          <form action="/" className="login__form">
+            <label className="login__input-wrapper">
+              <span className="visually-hidden">Введите e-mail</span>
+              <input type="text" name="email" className="login__input" placeholder="Введите e-mail" />
+            </label>
+            <label className="login__input-wrapper">
+              <span className="visually-hidden">Введите пароль</span>
+              <input type="text" name="password" className="login__input" placeholder="Введите пароль" />
+            </label>
+            <button className="login__sunbmit button button--form">Войти</button>
+          </form>
+        </div>
+        <div className="login__wrapper">
+          <h2 className="login__title">Регистрация</h2>
+          <form action="/" className="login__form">
+            <label className="login__input-wrapper">
+              <span className="visually-hidden">Введите e-mail</span>
+              <input type="text" name="email" className="login__input" placeholder="Введите e-mail" />
+            </label>
+            <label className="login__input-wrapper">
+              <span className="visually-hidden">Введите пароль</span>
+              <input type="text" name="password" className="login__input" placeholder="Введите пароль" />
+            </label>
+            <label className="login__input-wrapper">
+              <span className="visually-hidden">Подтвердите пароль</span>
+              <input type="text" name="repassword" className="login__input" placeholder="Подтвердите пароль" />
+            </label>
+            <button className="login__sunbmit button button--form">Регистрация</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  </main>;
+};
+
+export default Login;
