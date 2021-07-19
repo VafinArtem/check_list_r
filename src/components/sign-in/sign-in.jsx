ca45f652a3f42852e22af23304b6b6d4fdd63168
Vafin Artem <a.vafin85@gmail.com>
2021-07-19 23:44:08 +0500

    подготовка для показа сообщений под input

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -17,13 +17,18 @@ const SignIn = () => {
   const [toastMessage, setToastMessage] = useState(``);
   const [toastType, setToastType] = useState(``);
   const [validEmail, setValidEmail] = useState(false);
-  const [validPassword, setValidPassword] = useState(false);
+  const [validPassword, setValidPassword] = useState({
+    length: true,
+    match: true
+  });
+  // const [showMessage, setShowMessage] = useState(false);
 
   const loginRef = useRef();
   const passwordRef = useRef();
   const rePasswordRef = useRef();
 
   const handleEmailInput = () => {
+    // setShowMessage(false);
     if (loginRef.current.value.match(RegularExp.EMAIL) === null) {
       setValidEmail(false);
     } else {
@@ -32,6 +37,7 @@ const SignIn = () => {
   };
 
   const handlePasswordInput = () => {
+    // setShowMessage(false);
     if (passwordRef.current.value !== rePasswordRef.current.value || passwordRef.current.value.length < MIN_PASSWORD_LENGTH) {
       setValidPassword({
         length: passwordRef.current.value.length < 3,
@@ -51,10 +57,12 @@ const SignIn = () => {
       };
       dispatch(newUser(regData));
     } else if (!validEmail) {
+      setShowMessage(true);
       setToastMessage(ValidationMessages.WRONG_EMAIL);
       setToastType(ToastTypes.ERROR);
       setShowToast(true);
-    } else if (typeof (validPassword) === `object`) {
+    } else if (typeof (validPassword) === `object` || validPassword) {
+      setShowMessage(true);
       if (validPassword.match) {
         setToastMessage(ValidationMessages.MISMATCH_PASSWORDS);
       } else if (validPassword.length) {
@@ -90,15 +98,21 @@ const SignIn = () => {
       <form action="/" className="login__form" onSubmit={handleSubmit}>
         <label className="login__input-wrapper">
           <span className="visually-hidden">Введите e-mail</span>
-          <input ref={loginRef} type="email" name="email" className="login__input" placeholder="Введите e-mail" onInput={handleEmailInput} />
+          <input ref={loginRef} type="email" name="email" className={`login__input`} placeholder="Введите e-mail" onInput={handleEmailInput} />
+          {/* <input ref={loginRef} type="email" name="email" className={`login__input ${!validEmail && showMessage ? `login__input--error` : ``}`} placeholder="Введите e-mail" onInput={handleEmailInput} /> */}
+          {/* {!validEmail && showMessage ? <span className="login__message login__message--error">{ValidationMessages.WRONG_EMAIL}</span> : `` } */}
         </label>
         <label className="login__input-wrapper">
           <span className="visually-hidden">Введите пароль</span>
-          <input ref={passwordRef} type="password" name="password" className="login__input" placeholder="Введите пароль" onInput={handlePasswordInput} />
+          <input ref={passwordRef} type="password" name="password" className={`login__input`} placeholder="Введите пароль" onInput={handlePasswordInput} />
+          {/* <input ref={passwordRef} type="password" name="password" className={`login__input ${validPassword.length && showMessage ? `login__input--error` : ``}`} placeholder="Введите пароль" onInput={handlePasswordInput} /> */}
+          {/* {validPassword.length && showMessage ? <span className="login__message login__message--error">{ValidationMessages.PASSWORD_LENGTH}</span> : ``} */}
         </label>
         <label className="login__input-wrapper">
           <span className="visually-hidden">Подтвердите пароль</span>
-          <input ref={rePasswordRef} type="password" name="repassword" className="login__input" placeholder="Подтвердите пароль" onInput={handlePasswordInput} />
+          <input ref={rePasswordRef} type="password" name="repassword" className={`login__input`} placeholder="Подтвердите пароль" onInput={handlePasswordInput} />
+          {/* <input ref={rePasswordRef} type="password" name="repassword" className={`login__input ${validPassword.match && showMessage ? `login__input--error` : ``}`} placeholder="Подтвердите пароль" onInput={handlePasswordInput} /> */}
+          {/* {validPassword.match && showMessage ? <span className="login__message login__message--error">{ValidationMessages.MISMATCH_PASSWORDS}</span> : ``} */}
         </label>
         <button className="login__sunbmit button button--form">Регистрация</button>
       </form>
